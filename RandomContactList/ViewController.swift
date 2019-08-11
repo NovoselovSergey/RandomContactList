3461866f9fed552fa55908bb18eec0d2a112e8bc
Novoselov Sergey <s.m.novoselov@icloud.com>
2019-08-11 20:00:34 +0300

    Initial Commit

---
+++ RandomContactList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomContactList
+//
+//  Created by Sergey Novoselov on 11/08/2019.
+//  Copyright © 2019 Sergey Novoselov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
